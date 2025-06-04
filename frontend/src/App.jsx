5bbbfce68e53ae11c8235aa175f52e7ace1bf961
manoj singh <ma9j@hotmail.com>
2025-06-04 23:24:25 +0530

    update app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,15 +10,8 @@ function App() {
   useEffect(() => {
     dispatch(authCheck());
   }, [dispatch]);
-  const userId = useSelector((state) => state.user?.user?.id);
-  // console.log(userId);
 
   const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
-  // console.log(isAuthenticated);
-  // console.log(isAuthenticated);
-  // if (!isAuthenticated) {
-  //   return <div>Please log in to access the chat.</div>;
-  // }
 
   return (
     <>
